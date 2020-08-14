bda6bbc0061e9af06856df81abd2202e35c5c6c5
taunsquared <danbee@alum.mit.edu>
2020-08-15 00:10:51 +0100

    all python scripts

---
+++ CuttleShuttle_01_probabilityMOI.py
@@ -0,0 +1,279 @@
+# -*- coding: utf-8 -*-
+"""
+Paper: "An experimental method for evoking and characterizing dynamic color patterning of cuttlefish during prey capture" by Danbee Kim, Kendra Buresch, Roger Hanlon, and Adam R. Kampff
+Analysis: Probability of MOI
+
+Collects csv files of all moments of interest (MOI) from full, primary experimental dataset. 
+Calculate the probability of an MOI happening after 1st, 2nd, or 3rd previous MOI (i.e. catch happening after tentacle shot, tentacle shot happening after orientation, etc).
+Used in paper to calculate results section "Accuracy of Prey Capture". 
+
+Optional flags:
+"--display": False (default) or True
+"--saveVid": False (default) or True
+"--ROI": 'backOnly' (default) or 'entireCuttlefish'
+
+@author: Adam R Kampff and Danbee Kim
+"""
+import os
+import glob
+import datetime
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+import logging
+import pdb
+import argparse
+
+###################################
+# SET CURRENT WORKING DIRECTORY
+###################################
+cwd = os.getcwd()
+###################################
+# SCRIPT LOGGER
+###################################
+# grab today's date
+now = datetime.datetime.now()
+today_dateTime = now.strftime("%Y-%m-%d_%H-%M-%S")
+logging.basicConfig(filename="probabilityMOIs_" + today_dateTime + ".log", filemode='w', level=logging.INFO)
+###################################
+# FUNCTIONS
+###################################
+
+##########################################################
+#### MODIFY THIS FIRST FUNCTION BASED ON THE LOCATIONS OF:
+# 1) dataset_dir (folder with full, primary experimental dataset)
+# AND
+# 2) plots_dir (parent folder for all plots output by this script)
+### Current default uses a debugging source dataset
+##########################################################
+def load_data():
+    dataset_dir = r"C:\Users\taunsquared\Dropbox\CuttleShuttle\CuttleShuttle-VideoDataset-Raw"
+    plots_dir = r'C:\Users\taunsquared\Dropbox\CuttleShuttle\analysis\forPaper\plots'
+    return dataset_dir, plots_dir
+##########################################################
+
+def convert_timestamps_to_secs_from_start(allA_timestamps_dict, list_of_MOIs):
+    converted_dict = {}
+    for animal in allA_timestamps_dict:
+        converted_dict[animal] = {}
+        all_session_dates = []
+        all_session_lens = []
+        all_food_offerings = []
+        all_homebases = []
+        all_orientations = []
+        all_tentacle_shots = []
+        all_catches = []
+        all_mois = [all_food_offerings, all_homebases, all_orientations, all_tentacle_shots, all_catches]
+        for session_date in sorted(allA_timestamps_dict[animal].keys()):
+            all_session_dates.append(session_date)
+            start_ts = allA_timestamps_dict[animal][session_date]['session-vids'][0]
+            end_ts = allA_timestamps_dict[animal][session_date]['session-vids'][-1]
+            session_len = (end_ts - start_ts).total_seconds()
+            all_session_lens.append(session_len)
+            if len(allA_timestamps_dict[animal][session_date].keys())==1 and 'session-vids' in allA_timestamps_dict[animal][session_date]:
+                print("No moments of interest for animal {a} on {s}".format(a=animal,s=session_date))
+                for moi in range(len(list_of_MOIs)):
+                    all_mois[moi].append([])
+            else:
+                for moi in range(len(list_of_MOIs)):
+                    if list_of_MOIs[moi] in allA_timestamps_dict[animal][session_date]:
+                        this_session_mois = []
+                        if allA_timestamps_dict[animal][session_date][list_of_MOIs[moi]].size <= 1:
+                            timestamp = allA_timestamps_dict[animal][session_date][list_of_MOIs[moi]]
+                            time_diff = (timestamp - start_ts).total_seconds()
+                            this_session_mois.append(time_diff)
+                        else:
+                            for timestamp in allA_timestamps_dict[animal][session_date][list_of_MOIs[moi]]:
+                                time_diff = (timestamp - start_ts).total_seconds()
+                                this_session_mois.append(time_diff)
+                        all_mois[moi].append(this_session_mois)
+                    else:
+                        all_mois[moi].append([])
+        converted_dict[animal]['session dates'] = all_session_dates
+        converted_dict[animal]['session durations'] = all_session_lens
+        for moi in range(len(list_of_MOIs)):
+            converted_dict[animal][list_of_MOIs[moi]] = all_mois[moi]
+    return converted_dict
+
+def calc_prob_MOI_sequence(secsFromStart_dict, MOI, prevMOI):
+    MOIProb_dict = {}
+    for animal in secsFromStart_dict:
+        MOIProb_dict[animal] = {}
+        all_MOI = secsFromStart_dict[animal][MOI]
+        all_prevMOI = secsFromStart_dict[animal][prevMOI]
+        for day in range(len(all_MOI)):
+            if len(all_MOI[day])>0:
+                for moi in all_MOI[day]:
+                    if moi in all_prevMOI[day]:
+                        attempt_number = all_prevMOI[day].index(moi)
+                        MOIProb_dict[animal][attempt_number] = MOIProb_dict[animal].setdefault(attempt_number,0) + 1
+                    else:
+                        try:
+                            attempt_number = [x-moi>0 for x in all_prevMOI[day]].index(True)
+                            MOIProb_dict[animal][attempt_number] = MOIProb_dict[animal].setdefault(attempt_number,0) + 1
+                        except Exception:
+                            print('Error in {a}, day {d}'.format(a=animal, d=day))
+    return MOIProb_dict
+
+def plot_probMOIseq(probMOIseq_dict, MOI_str, prevMOI_str, plots_dir, todays_dt):
+    allA_probMOIseq = []
+    for animal in probMOIseq_dict:
+        all_numPrevMOI = sorted(probMOIseq_dict[animal].keys())
+        if len(all_numPrevMOI)>0:
+            max_numPrevMOI = max(all_numPrevMOI)
+            frequencies_to_plot = [0*x for x in range(max_numPrevMOI+1)]
+            for numPrevMOI in all_numPrevMOI:
+                frequencies_to_plot[numPrevMOI] = probMOIseq_dict[animal][numPrevMOI]
+        else:
+            print('No previous MOIs for {a}'.format(a=animal))
+        allA_probMOIseq.append(frequencies_to_plot)
+    mostTS_beforeCatch = max([len(x) for x in allA_probMOIseq])
+    for animal in range(len(allA_probMOIseq)):
+        pad_N = mostTS_beforeCatch-len(allA_probMOIseq[animal])
+        padded_animal = allA_probMOIseq[animal]+[0]*pad_N
+        allA_probMOIseq[animal] = padded_animal
+    # set figure save path and title
+    figure_name = 'Prob_MomentsOfInterestSeq_' + prevMOI_str + 'Before' + MOI_str + '_allAnimals_' + todays_dt + '.png'
+    figure_path = os.path.join(plots_dir, figure_name)
+    figure_title = 'Number of '+ prevMOI_str + ' before a ' + MOI_str + ' for all animals'
+    # set axes and other figure properties
+    ax = plt.figure(figsize=(16,9), dpi=200)
+    plt.suptitle(figure_title, fontsize=12, y=0.98)
+    x = np.arange(mostTS_beforeCatch)  # the label locations
+    plt.xticks(x)
+    plt.xlabel("{pM} before {M}".format(pM=prevMOI_str, M=MOI_str))
+    plt.ylabel("Frequency")
+    plt.grid(b=True, which='major', linestyle='-')
+    width = 0.1  # the width of the bars
+    # draw bars for 5 animals at each x tick
+    plt.bar(x - 2*width, allA_probMOIseq[0], width, label='L1-H2013-01')
+    plt.bar(x - width, allA_probMOIseq[1], width, label='L1-H2013-02')
+    plt.bar(x, allA_probMOIseq[2], width, label='L1-H2013-03')
+    plt.bar(x + width, allA_probMOIseq[3], width, label='L7-H2013-01')
+    plt.bar(x + 2*width, allA_probMOIseq[4], width, label='L7-H2013-02')
+    # Add legend
+    ax.legend()
+    # save and display fig
+    plt.savefig(figure_path)
+    plt.show(block=False)
+    plt.pause(1)
+    plt.close()
+
+##########################################################
+# BEGIN SCRIPT
+##########################################################
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(
+        description='''Probability of MOI.
+        Collects csv files of all moments of interest (MOI) from full, primary experimental dataset. 
+        Calculate the probability of an MOI happening after 1st, 2nd, or 3rd previous MOI (i.e. catch happening after tentacle shot, tentacle shot happening after orientation, etc).
+        Used in paper to calculate results section "Accuracy of Prey Capture". ''')
+    parser.add_argument("--a", nargs='?', default="check_string_for_empty")
+    parser.add_argument("--MOI", nargs='+', type=str, default='catches', help="Choose at least one from the following list of MOIs: catches (default), tentacle-shots, orients, homebase")
+    args = parser.parse_args()
+    ###################################
+    # SOURCE DATA AND OUTPUT FILE LOCATIONS 
+    ###################################
+    raw_dataset_folder, plots_folder = load_data()
+    logging.info('DATA FOLDER: %s \n PLOTS FOLDER: %s' % (raw_dataset_folder, plots_folder))
+    print('DATA FOLDER: %s \n PLOTS FOLDER: %s' % (raw_dataset_folder, plots_folder))
+    ###################################
+    # COLLECT CSV FILES FOR MOI
+    ###################################
+    animals = ['L1-H2013-01', 'L1-H2013-02', 'L1-H2013-03', 'L7-H2013-01', 'L7-H2013-02', 'L7-H2013-03']
+    allMOI_allA = {}
+    # extract data from csv and put into dictionary
+    logging.info('Extracting raw data from csv...')
+    print('Extracting raw data from csv...')
+    for animal in animals:
+        print('Working on animal {a}'.format(a=animal))
+        allMOI_allA[animal] = {}
+        MOI_homebase = glob.glob(raw_dataset_folder + os.sep + animal + os.sep + "*" + os.sep + "homebase*.csv")
+        MOI_orients = glob.glob(raw_dataset_folder + os.sep + animal + os.sep + "*" + os.sep + "orients*.csv")
+        MOI_TS = glob.glob(raw_dataset_folder + os.sep + animal + os.sep + "*" + os.sep + "tentacle_shots*.csv")
+        MOI_catches = glob.glob(raw_dataset_folder + os.sep + animal + os.sep + "*" + os.sep + "catches*.csv")
+        food_offerings = glob.glob(raw_dataset_folder + os.sep + animal + os.sep + "*" + os.sep + "food_available*.csv")
+        session_vids = glob.glob(raw_dataset_folder + os.sep + animal + os.sep + "*" + os.sep + "session_video*.csv")
+        all_MOI = [MOI_homebase, MOI_orients, MOI_TS, MOI_catches, food_offerings, session_vids]
+        for MOI_type in range(len(all_MOI)):
+            for csv_file in all_MOI[MOI_type]:
+                if os.path.getsize(csv_file)>0:
+                    csv_name = csv_file.split(os.sep)[-1]
+                    csv_date = csv_file.split(os.sep)[-2]
+                    csv_animal = csv_file.split(os.sep)[-3]
+                    current_dict_level = allMOI_allA[animal].setdefault(csv_date,{})
+                    # read csv file and convert timestamps into datetime objects
+                    str2date = lambda x: datetime.datetime.strptime(x.decode("utf-8").split('+')[0][:-1], '%Y-%m-%dT%H:%M:%S.%f')
+                    csv_MOI = np.genfromtxt(csv_file, dtype=None, delimiter=",", converters={0:str2date})
+                    if MOI_type == 0:
+                        allMOI_allA[animal][csv_date]['homebase'] = csv_MOI
+                    if MOI_type == 1:
+                        allMOI_allA[animal][csv_date]['orients'] = csv_MOI
+                    if MOI_type == 2:
+                        allMOI_allA[animal][csv_date]['tentacle-shots'] = csv_MOI
+                    if MOI_type == 3:
+                        allMOI_allA[animal][csv_date]['catches'] = csv_MOI
+                    if MOI_type == 4:
+                        allMOI_allA[animal][csv_date]['food-offerings'] = csv_MOI
+                    if MOI_type == 5:
+                        allMOI_allA[animal][csv_date]['session-vids'] = csv_MOI
+    print('Finished extracting csv data!')
+    logging.info('Finished extracting csv data!')
+    ###################################
+    # CONVERT TIMESTAMPS AND ANIMAL NUMBERS
+    ###################################
+    # convert timestamp obj's into ints (microseconds from start)
+    MOIs = ['food-offerings','homebase','orients','tentacle-shots','catches']
+    logging.info('Converting timestamps to microseconds from start...')
+    print('Converting timestamps to microseconds from start...')
+    allMOI_allA_converted = convert_timestamps_to_secs_from_start(allMOI_allA, MOIs)
+    # convert animal numbers into names
+    animal_names = {'L1-H2013-01':'Dora','L1-H2013-02':'Scar','L1-H2013-03':'Ender','L7-H2013-01':'Old Tom','L7-H2013-02':'Plato','L7-H2013-03':'Blaise'}
+    ##################################################################################
+    ### ---- PROBABILITY OF MOIS HAPPENING AFTER 1ST/2ND/3RD/4TH PREVIOUS MOI ---- ###
+    ##################################################################################
+    MOI_to_prevMOI = {'catches': 'tentacle-shots', 'tentacle-shots': 'orients', 'orients': 'food-offerings', 'homebase': 'catches'}
+    MOIs = args.MOI
+    for MOI in MOIs:
+        logging.info('Calculating probability of {m} happening after {pm}...'.format(m=MOI, pm=MOI_to_prevMOI[MOI]))
+        print('Calculating probability of {m} happening after {pm}...'.format(m=MOI, pm=MOI_to_prevMOI[MOI]))
+        allA_probMOI = calc_prob_MOI_sequence(allMOI_allA_converted, MOI, MOI_to_prevMOI[MOI])
+        # summary stats
+        first_MOI_prob = {}
+        second_MOI_prob = {}
+        third_MOI_prob = {}
+        for animal in allA_probMOI:
+            if bool(allA_probMOI[animal]):
+                total_prevMOIs = sum(allA_probMOI[animal].values())
+                first_attempt = allA_probMOI[animal].get(0, 0)
+                first_MOI_prob[animal] = first_attempt/total_prevMOIs
+                two_attempts_or_less = allA_probMOI[animal].get(0, 0) + allA_probMOI[animal].get(1, 0)
+                second_MOI_prob[animal] = two_attempts_or_less/total_prevMOIs
+                three_attempts_or_less = allA_probMOI[animal].get(0, 0) + allA_probMOI[animal].get(1, 0) + allA_probMOI[animal].get(2, 0)
+                third_MOI_prob[animal] = three_attempts_or_less/total_prevMOIs
+        for animal in first_MOI_prob:
+            logging.info('Probability of animal {a} making {m} after first {pm}: {prob:.4f}'.format(a=animal, m=MOI, pm=MOI_to_prevMOI[MOI], prob=first_MOI_prob[animal]))
+            print('Probability of animal {a} making {m} after first {pm}: {prob:.4f}'.format(a=animal, m=MOI, pm=MOI_to_prevMOI[MOI], prob=first_MOI_prob[animal]))
+        mean_first_attempt_success = np.mean([x for x in first_MOI_prob.values()])
+        var_first_attempt_success = np.var([x for x in first_MOI_prob.values()])
+        logging.info('Mean probability of all animals making {m} after first {pm}: {mean:.4f} +/- {var:.4f}'.format(m=MOI, pm=MOI_to_prevMOI[MOI], mean=mean_first_attempt_success, var=var_first_attempt_success))
+        print('Mean probability of all animals making {m} after first {pm}: {mean:.4f} +/- {var:.4f}'.format(m=MOI, pm=MOI_to_prevMOI[MOI], mean=mean_first_attempt_success, var=var_first_attempt_success))
+        for animal in second_MOI_prob:
+            logging.info('Probability of animal {a} making {m} after second {pm}: {prob:.4f}'.format(a=animal, m=MOI, pm=MOI_to_prevMOI[MOI], prob=second_MOI_prob[animal]))
+            print('Probability of animal {a} making {m} after second {pm}: {prob:.4f}'.format(a=animal, m=MOI, pm=MOI_to_prevMOI[MOI], prob=second_MOI_prob[animal]))
+        mean_second_attempt_success = np.mean([x for x in second_MOI_prob.values()])
+        var_second_attempt_success = np.var([x for x in second_MOI_prob.values()])
+        logging.info('Mean probability of all animals making {m} after second {pm}: {mean:.4f} +/- {var:.4f}'.format(m=MOI, pm=MOI_to_prevMOI[MOI], mean=mean_second_attempt_success, var=var_second_attempt_success))
+        print('Mean probability of all animals making {m} after second {pm}: {mean:.4f} +/- {var:.4f}'.format(m=MOI, pm=MOI_to_prevMOI[MOI], mean=mean_second_attempt_success, var=var_second_attempt_success))
+        for animal in third_MOI_prob:
+            logging.info('Probability of animal {a} making {m} after third {pm}: {prob:.4f}'.format(a=animal, m=MOI, pm=MOI_to_prevMOI[MOI], prob=third_MOI_prob[animal]))
+            print('Probability of animal {a} making {m} after third {pm}: {prob:.4f}'.format(a=animal, m=MOI, pm=MOI_to_prevMOI[MOI], prob=third_MOI_prob[animal]))
+        mean_third_attempt_success = np.mean([x for x in third_MOI_prob.values()])
+        var_third_attempt_success = np.var([x for x in third_MOI_prob.values()])
+        logging.info('Mean probability of all animals making {m} after third {pm}: {mean:.4f} +/- {var:.4f}'.format(m=MOI, pm=MOI_to_prevMOI[MOI], mean=mean_third_attempt_success, var=var_third_attempt_success))
+        print('Mean probability of all animals making {m} after third {pm}: {mean:.4f} +/- {var:.4f}'.format(m=MOI, pm=MOI_to_prevMOI[MOI], mean=mean_third_attempt_success, var=var_third_attempt_success))
+        # plot summary of catch accuracy
+        plot_probMOIseq(allA_probMOI, MOI, MOI_to_prevMOI[MOI], plots_folder, today_dateTime)
+
+# FIN
---
+++ CuttleShuttle_02_ProcessCuttlePython_genBandEnergies.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+"""
+Paper: "An experimental method for evoking and characterizing dynamic color patterning of cuttlefish during prey capture" by Danbee Kim, Kendra Buresch, Roger Hanlon, and Adam R. Kampff
+Analysis: Step 1 of process_cuttle_python Python Workflow
+
+Processes cropped and aligned video of cuttlefish, measures contrast (aka granularity) in multiple spatial bands. 
+Generate intermediate files with power at 7 spatial frequency bands for each frame.
+
+Optional flags:
+"--display": False (default) or True
+"--saveVid": False (default) or True
+"--ROI": 'backOnly' (default) or 'entireCuttlefish'
+
+@author: Adam R Kampff and Danbee Kim
+"""
+import os
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import datetime
+import logging
+import pdb
+import argparse
+
+###################################
+# SET CURRENT WORKING DIRECTORY
+###################################
+cwd = os.getcwd()
+###################################
+# SCRIPT LOGGER
+###################################
+# grab today's date
+now = datetime.datetime.now()
+today_dateTime = now.strftime("%Y-%m-%d_%H-%M-%S")
+logging.basicConfig(filename="process_cuttle_python_01_" + today_dateTime + ".log", filemode='w', level=logging.INFO)
+###################################
+# FUNCTIONS
+###################################
+
+##########################################################
+#### MODIFY THIS FIRST FUNCTION BASED ON THE LOCATIONS OF:
+# 1) video_dir (parent folder with all TGB videos)
+# AND
+# 2) plots_dir (parent folder for all plots output by this script)
+### Current default uses a debugging source dataset
+##########################################################
+def load_data():
+    video_dir = r'C:\Users\taunsquared\Dropbox\CuttleShuttle\CuttleShuttle-ManuallyAligned\CroppedAligned\MantleZoom\TentacleShots'
+    plots_dir = r'C:\Users\taunsquared\Dropbox\CuttleShuttle\analysis\WoodsHoleAnalysis\draftPlots\intermediates'
+    return video_dir, plots_dir
+##########################################################
+
+def genBandMasks(number_bands, crop_roi):
+    # expected format for crop_roi = [roi_ul_x, roi_ul_y, roi_lr_x, roi_lr_y]
+    # Measure ROI size
+    roi_width = crop_roi[2] - crop_roi[0]
+    roi_height = crop_roi[3] - crop_roi[1]
+    # Generate band masks
+    StartX = -np.round((roi_width+1)/2)
+    EndX = StartX + roi_width
+    StartY = -np.round((roi_height+1)/2)
+    EndY = StartY + roi_height
+    X,Y = np.meshgrid(np.arange(StartX, EndX), np.arange(StartY, EndY).T)
+    bands = np.arange(number_bands,0, -1)
+    radii = np.power(1/2, bands)
+    band_masks = np.zeros((roi_height, roi_width, number_bands))
+    for i in np.arange(number_bands):
+        if (i == 0):
+            band_screen = ((X/roi_width)**2 + (Y/roi_height)**2) <= radii[i]**2
+        else:
+            band_screen = (((X/roi_width)**2 + (Y/roi_height)**2) > radii[i-1]**2) & (((X/roi_width)**2 + (Y/roi_height)**2) <= radii[i]**2)
+        #plt.imshow(band_screen)
+        #plt.show()
+        band_masks[:,:,i] = band_screen
+    return band_masks
+
+def computeFilteredVid(N_frames, N_bands, TS_video, TS_video_path, crop_roi, band_masks, display_bool, save_bool, save_folder):
+    # expected format for crop_roi = [roi_ul_x, roi_ul_y, roi_lr_x, roi_lr_y]
+    # Measure ROI size
+    roi_width = crop_roi[2] - crop_roi[0]
+    roi_height = crop_roi[3] - crop_roi[1]
+    # Loop through all frames of TS_video and process
+    band_energies = np.zeros((N_frames, N_bands))
+    #N_frames = 50 # ...for debugging
+    for frame in range(0, N_frames):
+        # Read current frame
+        success, image = TS_video.read()
+        # Convert to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Crop
+        crop = gray[crop_roi[1]:crop_roi[3], crop_roi[0]:crop_roi[2]]
+        crop = np.float32(crop)
+        # Transform to Weber contrasts
+        mean_crop = np.mean(crop[:])
+        weber = (crop-mean_crop)/mean_crop
+        # 2D FFT and power spectrum
+        fft = np.fft.fft2(weber)
+        fft_centered = np.fft.fftshift(fft)
+        spectrum = np.real(fft_centered * np.conj(fft_centered))
+        # Apply band masks
+        band_energy = np.zeros(N_bands)
+        for i in np.arange(N_bands):
+            band_energy[i] = np.sum(band_masks[:,:,i] * spectrum)
+        band_energies[frame, :] = band_energy / sum(band_energy)
+        # If displaying or saving, compute filtered images via inverse FFT
+        if display_bool or save_bool:
+            # setup output video
+            output_video_name = os.path.basename(TS_video_path)[:-4] + "_filteredVid.avi"
+            output_video_path = os.path.join(save_folder, output_video_name)
+            output_video_size = (1280, 400)
+            fourcc = cv2.VideoWriter_fourcc('F','M','P','4')
+            output_video = cv2.VideoWriter(output_video_path, fourcc, 30, output_video_size, False)
+            # load output frames
+            filtered_images = np.zeros((roi_width, roi_height * (N_bands + 1)))
+            filtered_images[:, 0:roi_height] = crop.T/255
+            for i in np.arange(N_bands):
+                filtered_fft = np.fft.ifftshift(band_masks[:,:,i] * fft_centered)
+                filtered_image = np.real(np.fft.ifft2(filtered_fft))
+                offset = (roi_height * (i + 1))
+                filtered_images[:, offset:(offset+roi_height)] = filtered_image.T + 0.5
+            filtered_images_small = cv2.resize(filtered_images, output_video_size)
+        # If displaying, display
+        if display_bool:
+            #ret = cv2.imshow("Display", spectrum/100000)
+            ret = cv2.imshow("Display", filtered_images_small)
+            ret = cv2.waitKey(1)
+        # If saving, setup and save output video
+        if save_bool:
+            # save output video
+            filtered_images_small_u8 = (filtered_images_small * 200)
+            filtered_images_small_u8[filtered_images_small_u8 > 255] = 255
+            filtered_images_small_u8[filtered_images_small_u8 < 0] = 0
+            filtered_images_small_u8 = np.uint8(filtered_images_small_u8)
+            output_video.write(filtered_images_small_u8)
+    # Save band energies as intermediate file
+    data_name = os.path.basename(TS_video_path)[:-4] + "_bandEnergies.npy"
+    data_path = os.path.join(save_folder, data_name)
+    np.save(data_path, band_energies)
+    # Plot band energies for entire TS_video
+    # set fig path and title
+    shot_type = os.path.basename(TS_video_path).split('_')[-2]
+    animal_name = os.path.basename(TS_video_path).split('_')[1]
+    figure_name = os.path.basename(TS_video_path)[:-4] + "_PowerFreqBandPlot.png"
+    figure_path = os.path.join(save_folder, figure_name)
+    figure_title = "Energy of each frequency band during tentacle shot (shot occurs at frame 180) \n Animal: {a}, Tentacle Shot type: {s}".format(a=animal_name, s=shot_type)
+    # draw fig
+    plt.figure(figsize=(16,8), dpi=200)
+    plt.suptitle(figure_title, fontsize=12, y=0.99)
+    plt.plot(band_energies)
+    labels = ['band 0', 'band 1','band 2','band 3','band 4','band 5','band 6']
+    plt.legend(labels)
+    plt.ylabel('Power')
+    plt.xlabel('Frame number')
+    # save and show
+    plt.savefig(figure_path)
+    plt.show(block=False)
+    plt.pause(1)
+    plt.close()
+    # Cleanup
+    if save_bool:
+        output_video.release()
+    if display_bool:
+        cv2.destroyAllWindows()
+
+##########################################################
+# BEGIN SCRIPT
+##########################################################
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(
+        description='''Step 1 of process_cuttle_python Python Workflow.
+        Processes cropped and aligned video of cuttlefish, measures contrast (aka granularity) in multiple spatial bands. 
+        Generate intermediate files with power at 7 spatial frequency bands for each frame.''')
+    parser.add_argument("--a", nargs='?', default="check_string_for_empty")
+    parser.add_argument("--display", nargs=1, default=False, help="Set to 'True' to display frame by frame analysis as script is running.")
+    parser.add_argument("--saveVid", nargs=1, default=False, help="Set to 'True' to save video of bandpass filtered images at each spatial frequency.")
+    parser.add_argument("--ROI", nargs=1, default='backOnly', help="Change which part of the video frame to analyse. Options: 'backOnly' (default), 'entireCuttlefish'")
+    args = parser.parse_args()
+    ###################################
+    # SOURCE DATA AND OUTPUT FILE LOCATIONS 
+    ###################################
+    video_folder, plots_folder = load_data()
+    logging.info('DATA FOLDER: %s \n PLOTS FOLDER: %s' % (video_folder, plots_folder))
+    print('DATA FOLDER: %s \n PLOTS FOLDER: %s' % (video_folder, plots_folder))
+    ###################################
+    # DISPLAY AND SAVE TOGGLES
+    ###################################
+    display = args.display
+    save = args.saveVid
+    ###################################
+    # SPECIFY CROP ROI (upper left pixel (x,y) and lower right pixel (x,y))
+    ###################################
+    # Only back of cuttlefish
+    if args.ROI == 'backOnly':
+        roi_ul_x = 600
+        roi_ul_y = 350
+        roi_lr_x = 1400
+        roi_lr_y = 750
+    # Entire cuttlefish
+    if args.ROI == 'entireCuttlefish':
+        roi_ul_x = 300
+        roi_ul_y = 100
+        roi_lr_x = 1700
+        roi_lr_y = 1000
+    CropRoi = [roi_ul_x, roi_ul_y, roi_lr_x, roi_lr_y]
+    ###################################
+    # COLLECT VIDEOS FROM VIDEO_FOLDER
+    ###################################
+    all_vids = glob.glob(video_folder + os.sep + "*.avi")
+    # Loop through videos and process
+    for video_path in all_vids: 
+        # Open video
+        video = cv2.VideoCapture(video_path)
+        # Read video parameters
+        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        # Generate band masks
+        NumBands = 7
+        BandMasks = genBandMasks(NumBands, CropRoi)
+        # If displaying, open display window
+        if display:
+            cv2.namedWindow("Display")
+        computeFilteredVid(num_frames, NumBands, video, video_path, CropRoi, BandMasks, display, save, plots_folder)
+        video.release()
+
+#FIN
